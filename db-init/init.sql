657fd12ee46cc91904f72a910de6bdcc13f59d80
Manish Manandhar <mdhrmanish7@gmail.com>
2026-09-09 19:37:42 +0545

    fixed workflows and db syntax error

--- db-init/init.sql
+++ db-init/init.sql
@@ -4,7 +4,7 @@ USE mydb;
 
 CREATE TABLE IF NOT EXISTS users (
  id INT AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(100) NOT NULL,   
+ name VARCHAR(100) NOT NULL 
 );
 
 INSERT into users (name) values ('John Doe'), ('Jane Smith'), ('Alice Johnson');
